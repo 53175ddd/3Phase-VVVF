0e50875f915c113e3c03361be12ad536410c2380
Nch MOSFET <91242561+Nch-MOSFET@users.noreply.github.com>
2022-06-10 21:38:14 +0900

    Update make3Phase.ino

--- Arduino/make3Phase/make3Phase.ino
+++ Arduino/make3Phase/make3Phase.ino
@@ -1,6 +1,9 @@
 //8bit幅の三相正弦波をCSV形式で出力します:
+
 #define BaudRate        115200
-#define waveVol         1000
+#define waveVol         1024       //正弦波1周期のステップ数を指定:
+#define triWaveFreq     12         //搬送波を正弦波の何倍の周波数にするかを指定、必ず4の倍数に:
+
 
 float Tau = 2 * PI;
 
@@ -15,35 +18,51 @@ void setup() {
   for(uint16_t i = 0; i < waveVol; i++) {
     float theta = (i * Tau) / waveVol;
     uSin[i] = (sin(theta) * 126) + 128;
+  }
+  for(uint16_t i = 0; i < waveVol; i++) {
+    float theta = ((i * Tau) / waveVol) + (Tau / 3);
+    vSin[i] = (sin(theta) * 126) + 128;
+  }
+  for(uint16_t i = 0; i < waveVol; i++) {
+    float theta = ((i * Tau) / waveVol) + ((Tau * 2) / 3);
+    wSin[i] = (sin(theta) * 126) + 128;
+  }
+
+  const uint16_t offset = waveVol / (triWaveFreq * 4);
+
+  for(uint16_t i = 0; i < offset; i++) {
+    cTri[i] = map(i, 0, offset, 128, 255);
+  }
+
+  for(uint16_t i = offset; i < (offset * 3); i++) {
+    cTri[i] = map(i - offset, 0, (offset * 2), 255, 0);
+  }
+
+  for(uint16_t i = (offset * 3); i < (offset * 4); i++) {
+    cTri[i] = map(i - (offset * 3), 0, offset, 0, 127);
+  }
+
+  for(uint16_t i = 0; i < (waveVol - (offset * 4)); i++) {
+    cTri[i + (offset * 4)] = cTri[i];
+  }
+
+  for(uint16_t i = 0; i < waveVol; i++) {
     Serial.print(String(uSin[i]) + ",");
   }
   Serial.println();
   for(uint16_t i = 0; i < waveVol; i++) {
-    float theta = ((i * Tau) / waveVol) + (Tau / 3);
-    vSin[i] = (sin(theta) * 126) + 128;
     Serial.print(String(vSin[i]) + ",");
   }
   Serial.println();
   for(uint16_t i = 0; i < waveVol; i++) {
-    float theta = ((i * Tau) / waveVol) + ((Tau * 2) / 3);
-    wSin[i] = (sin(theta) * 126) + 128;
     Serial.print(String(wSin[i]) + ",");
   }
   Serial.println();
-  for(uint16_t i = 0; i < 10; i++) {
-    for(uint8_t o = 0; o < 50; o++) {
-      uint16_t phase = o + 20 * i * 2;
-      cTri[phase] = (255 / 50) * o;
-    Serial.print(String(cTri[phase]) + ",");
-    }
-    for(uint8_t o = 50; o > 0; o--) {
-      uint16_t phase = (o + 1) + 20 * i * 2;
-      cTri[phase] = (255 / 50) * o;
-    Serial.print(String(cTri[phase]) + ",");
-    }
+  for(uint16_t i = 0; i < waveVol; i++) {
+    Serial.print(String(cTri[i]) + ",");
   }
+  Serial.println();
 }
 
 void loop() {
-  
 }
